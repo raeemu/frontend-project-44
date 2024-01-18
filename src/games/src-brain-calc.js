1a8fdf0d98a5a50250e234a7058b97c4724145f9
raeemu <suerbay55@gmail.com>
2024-01-18 13:26:53 +0300

    no eval ;(

--- src/games/src-brain-calc.js
+++ src/games/src-brain-calc.js
@@ -1,15 +1,13 @@
-import { getRandomIntInclusive } from '../helpers.js';
+import { calcHelper, getRandomIntInclusive } from '../helpers.js';
 import gameEngine from '../index.js';
 
 const calcualtor = () => {
   const question = 'What is the result of the expression?';
   const calcualtorLogic = () => {
-    const actions = '+-*';
     const numero1 = getRandomIntInclusive(1, 25);
     const numero2 = getRandomIntInclusive(1, 25);
-    const action = actions[getRandomIntInclusive(0, 2)];
+    const [trueAnswer, action] = calcHelper(numero1, numero2);
     const finalExpression = `${numero1} ${action} ${numero2}`;
-    const trueAnswer = eval(finalExpression).toString();
     return [finalExpression, trueAnswer];
   };
   gameEngine(calcualtorLogic, question);
--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,15 @@ export const getRandomIntInclusive = (x, y) => {
 };
 
 export const gcd = (x, y) => (y === 0 ? x : gcd(y, x % y));
+
+export const calcHelper = (x, y) => {
+  const actions = '+-*';
+  const action = getRandomIntInclusive(0, 2);
+  if (action === 0) {
+    return [(x + y).toString(), actions[0]];
+  }
+  if (action === 1) {
+    return [(x - y).toString(), actions[1]];
+  }
+  return [(x * y).toString(), actions[2]];
+};
