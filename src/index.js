b74716441ab8e1073635b4db8c5a4aeb2a42f069
raeemu <suerbay55@gmail.com>
2024-01-18 12:48:28 +0300

    ????

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const gameEngine = (logic, question) => {
   for (let i = 0; i < 3; i += 1) {
     const [finalExpression, trueAnswer] = logic();
     const answer = readlineSync.question(`Question: ${finalExpression}
-  Your answer: `);
+Your answer: `);
     if (answer !== trueAnswer) {
-      console.log(`${answer} is a wrong answer ;(. The correct answer was ${trueAnswer}, let's try again, ${name}!`);
+      console.log(`${answer} is a wrong answer ;(. The correct answer was ${trueAnswer}, 
+Let's try again, ${name}!`);
       return;
     } console.log('Correct!');
   }
